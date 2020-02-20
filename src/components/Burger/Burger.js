19b8477d0bd9734bf224a7b6aa0febc5af21dd91
Anas452 <anasm9877@gmail.com>
2020-02-20 23:52:33 +0500

    burger UI setup

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../../css/burger.css'
+import BurgerIngredient from './Burgeringredient/Burgeringredient'
 
 const burger = (props) => {
     return(  
-        <div>
-
+        <div className='Burger'>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
         </div>
     )
 }
