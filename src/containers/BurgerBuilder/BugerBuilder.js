c55590b60779faf9bb27c92b2b4478f783ae14e1
Anas452 <anasm9877@gmail.com>
2020-03-15 16:04:00 +0500

    redux to checkout

--- src/containers/BurgerBuilder/BugerBuilder.js
+++ src/containers/BurgerBuilder/BugerBuilder.js
@@ -121,18 +121,15 @@ class BurgerBuilder extends Component{
     //         console.log(error)
     //     })
 
-    const queryParams = [];
+    // const queryParams = [];
 
-    for (let i in this.state.ingredients){
-        queryParams.push(encodeURIComponent(i) + '=' +encodeURIComponent(this.state.ingredients[i]))
-    }
-    queryParams.push('price=' + this.props.price);
-    const queryString = queryParams.join('&');
+    // for (let i in this.state.ingredients){
+    //     queryParams.push(encodeURIComponent(i) + '=' +encodeURIComponent(this.state.ingredients[i]))
+    // }
+    // queryParams.push('price=' + this.props.price);
+    // const queryString = queryParams.join('&');
 
-    this.props.history.push({
-        pathname : '/checkout',
-        search : '?' + queryString
-    })
+    this.props.history.push('/checkout')
 }
 
     render(){
